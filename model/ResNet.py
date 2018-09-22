785228118d9f765971d1306d0a50657c2b1c160d
shamoji101 <4417619@ed.tus.ac.jp>
2018-09-23 01:46:45 +0900

    bugfix:ResNet34 & 18-mobile typo

--- model/ResNet.py
+++ model/ResNet.py
@@ -273,19 +273,19 @@ class ResNet34_forCIFAR10(nn.Module):
         self.Conv3_3 = ResBlock(128, Kernel_size=3, Padding=1)        
         self.Conv3_4 = ResBlock(128, Kernel_size=3, Padding=1)
         
-        self.SecondConv = IncreaseChannel_ResBlock(64,128, kernel_size=3, stride=2, padding=1)
+        self.ThirdConv = IncreaseChannel_ResBlock(128,256, kernel_size=3, stride=2, padding=1)
         
-        self.Conv3_1 = ResBlock(128, Kernel_size=3, Padding=1)
-        self.Conv3_2 = ResBlock(128, Kernel_size=3, Padding=1)
+        self.Conv4_1 = ResBlock(256, Kernel_size=3, Padding=1)
+        self.Conv4_2 = ResBlock(256, Kernel_size=3, Padding=1)
         self.Conv4_3 = ResBlock(256, Kernel_size=3, Padding=1)
         self.Conv4_4 = ResBlock(256, Kernel_size=3, Padding=1)
         self.Conv4_5 = ResBlock(256, Kernel_size=3, Padding=1)
         self.Conv4_6 = ResBlock(256, Kernel_size=3, Padding=1)
         
-        self.SecondConv = IncreaseChannel_ResBlock(64,128, kernel_size=3, stride=2, padding=1)
+        self.LastConv = IncreaseChannel_ResBlock(256,512, kernel_size=3, stride=2, padding=1)
         
-        self.Conv3_1 = ResBlock(128, Kernel_size=3, Padding=1)
-        self.Conv3_2 = ResBlock(128, Kernel_size=3, Padding=1)
+        self.Conv5_1 = ResBlock(512, Kernel_size=3, Padding=1)
+        self.Conv5_2 = ResBlock(512, Kernel_size=3, Padding=1)
         self.Conv5_3 = ResBlock(512, Kernel_size=3, Padding=1)
         
         self.GAP = nn.AvgPool2d(4)
@@ -329,3 +329,68 @@ class ResNet34_forCIFAR10(nn.Module):
         
         return x
     
+class ResNet18_ForCIFAR10_mobile(nn.Module):
+    """
+    this model is not powerful, but very highspeed.
+    """
+    def __init__(self):
+        
+        super(ResNet18_ForCIFAR10_mobile, self).__init__()
+
+        self.FirstConv = IncreaseChannel_ResBlock(3,64, kernel_size=5, stride=2, padding=2)
+
+        self.Conv2_1 = ResBlock(64, Kernel_size=3, Padding=1)
+        self.Conv2_2 = ResBlock(64, Kernel_size=3, Padding=1)
+        self.Conv2_3 = ResBlock(64, Kernel_size=3, Padding=1)
+
+        self.SecondConv = IncreaseChannel_ResBlock(64,128, kernel_size=3, stride=2, padding=1)
+        
+        self.Conv3_1 = ResBlock(128, Kernel_size=3, Padding=1)
+        self.Conv3_2 = ResBlock(128, Kernel_size=3, Padding=1)
+        
+        self.ThirdConv = IncreaseChannel_ResBlock(128,256, kernel_size=3, stride=2, padding=1)
+        
+        self.Conv4_1 = ResBlock(256, Kernel_size=3, Padding=1)
+        self.Conv4_2 = ResBlock(256, Kernel_size=3, Padding=1)
+        
+        self.LastConv = IncreaseChannel_ResBlock(256,512, kernel_size=3, stride=2, padding=1)
+        
+        self.Conv5_1 = ResBlock(512, Kernel_size=3, Padding=1)
+        self.Conv5_2 = ResBlock(512, Kernel_size=3, Padding=1)
+
+
+        self.GAP = nn.AvgPool2d(2)
+        self.Dense = nn.Linear(512,10)
+    
+        
+    def forward(self, x):
+        
+        
+        x = self.FirstConv(x)
+        
+        x = self.Conv2_1(x)
+        x = self.Conv2_2(x)
+        x = self.Conv2_3(x)
+        
+        x = self.SecondConv(x)
+        
+        x = self.Conv3_1(x)
+        x = self.Conv3_2(x)
+
+        
+        x = self.ThirdConv(x)
+        
+        x = self.Conv4_1(x)
+        x = self.Conv4_2(x)
+
+        x = self.LastConv(x)
+        
+        x = self.Conv5_1(x)
+        x = self.Conv5_2(x)
+        
+        x = self.GAP(x)
+        x = x.view(-1,512)
+        x = self.Dense(x)
+        
+        return x
+   
