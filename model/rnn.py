d16e25e5034d241b75bece5e20b3421a81a94b5c
shamoji101 <4417619@ed.tus.ac.jp>
2018-10-07 22:07:02 +0900

    new: add LSTM cell model

--- model/rnn.py
+++ model/rnn.py
@@ -23,4 +23,57 @@ class RNN_Cell(nn.Module):
         
         return hidden
 
+
+class LSTM_Cell(nn.Module):
+
+    def __init__(self, input_size, hidden_size):
+        super(LSTM_Cell, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.forgetunit_input_FC_x = nn.Linear(input_size, hidden_size)
+        self.forgetunit_input_FC_h = nn.Linear(hidden_size, hidden_size)
         
+        self.inputunit_input_FC_x = nn.Linear(input_size, hidden_size)
+        self.inputunit_input_FC_h = nn.Linear(hidden_size, hidden_size)
+        
+        self.updateunit_input_FC_x = nn.Linear(input_size, hidden_size)
+        self.updateunit_input_FC_h = nn.Linear(hidden_size, hidden_size)
+        
+        self.outputunit_input_FC_x = nn.Linear(input_size, hidden_size)
+        self.outputunit_input_FC_h = nn.Linear(hidden_size, hidden_size)
+        
+    def forward(self, input_x, hidden, cell):
+        
+        input_1=self.forgetunit_input_FC_x(input_x)
+        input_2=self.forgetunit_input_FC_h(hidden)
+        forget_x = input_1 + input_2
+        forget_x = F.sigmoid(forget_x)
+        cell_forget = cell * forget_x
+
+        input_1=self.inputunit_input_FC_x(input_x)
+        input_2=self.inputunit_input_FC_h(hidden)
+
+        input_x = input_1 + input_2
+        input_x = F.sigmoid(input_x)
+
+        input_1=self.updateunit_input_FC_x(input_x)
+        input_2=self.updateunit_input_FC_h(hidden)
+        
+        update_x = input_1 + input_2
+        update_x = F.tanh(update_x)
+
+        new_information = input_x * update_x
+        cell_next = cell_forget + new_information
+
+        input_1=self.outputunit_input_FC_x(input_x)
+        input_2=self.outputunit_input_FC_h(hidden)
+        
+        output_x = input_1 + input_2
+        output_x = F.sigmoid(output_x)
+        
+        hidden_next = output_x * F.tanh(cell_next)
+
+
+        return hidden_next,cell_next
